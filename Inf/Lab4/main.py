92369b26c184df886e9ab2e6b442252af2e6aab4
naumovpavel <naumovpasha193@gmail.com>
2022-11-30 11:28:52 +0300

    Inf Lab3&4 fix

--- Inf/Lab4/main.py
+++ Inf/Lab4/main.py
@@ -1,4 +1,4 @@
-from time import time
+import time
 
 from raw_parse import *
 from reg_parse import *
@@ -27,21 +27,22 @@ def task3(data, xml_f):
 
 
 def bench(data, xml_f, xml_lib_f):
-    start = time()
+    start = time.process_time()
+    time.sleep(25)
     for j in range(100):
         del_file_content(xml_f)
         task1(data, xml_f)
-    print(f"Task 1 time: {time() - start}")
-    start = time()
+    print(f"Task 1 time: {time.process_time()    - start}")
+    start = time.monotonic()
     for j in range(100):
         del_file_content(xml_lib_f)
         lib_parse(data, xml_lib_f)
-    print(f"Task 2 time: {time() - start}")
-    start = time()
+    print(f"Task 2 time: {time.monotonic() - start}")
+    start = time.monotonic()
     for j in range(100):
         del_file_content(xml_f)
         task3(data, xml_f)
-    print(f"Task 3 time: {time() - start}")
+    print(f"Task 3 time: {time.monotonic() - start}")
 
 
 data = open("data.json", "r+", encoding="utf-8").read()
@@ -50,7 +51,7 @@ xml_lib_f = open("data_lib.xml", "w+", encoding="utf-8")
 proto_f = open("SerializedToProto", "wb")
 
 
-task1(data, open("data.xml", "w+"))
+task1(data, open("data.xml", "w+", encoding="utf-8"))
 lib_parse(data, xml_lib_f)
 task3(data, open("data.xml", "w+", encoding="utf-8"))
 to_proto(json.loads(data), proto_f)
